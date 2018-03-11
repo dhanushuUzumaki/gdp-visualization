874eb470f40523d892f38693cfc4d3b2c25d31b0
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-03-11 22:49:53 +0530

    hover events

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const data = [10,34,86, 12, 65];
 const chart = d3.select('.chart');
 
+const plotData = (data) => {
+  console.log(data);
+  chart.selectAll('div')
+      .data(data)
+      .enter()
+      .append('div')
+      .style('width', d => `${d[1]/100}%`)      
+      .text(d => d[0])
+      .on("mouseover", (d) => {
+        console.log(d);
+      })
+      .on("mouseout", () => {
+        console.log('outside');
+      });
+}
+
 const getGDPData = () => {
   return fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(response => {
@@ -11,13 +27,8 @@ const getGDPData = () => {
 const fetchData = async () => {
   try {
     const response = await getGDPData();
-    console.log(response.data);
-    chart.selectAll('div')
-      .data(response.data)
-      .enter()
-      .append('div')
-      .style('width', d => `${d[1]/100}%`)      
-      .text(d => d[0]);
+    console.log(response);
+    plotData(response.data);
   } catch (e) {
     console.error(e);
   }
